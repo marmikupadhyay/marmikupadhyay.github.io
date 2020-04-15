b6b52098ffd92c4b7aefe677ae2f77fcba4ac03a
marmikupadhyay <62810047+marmikupadhyay@users.noreply.github.com>
2020-04-15 21:57:12 +0530

    Added Hacker Mode

--- deltaTask1/hacker/main.js
+++ deltaTask1/hacker/main.js
@@ -1,36 +1,56 @@
-var items = [];
-
 var scores = [];
 putScores();
+var items = [];
+var tapSound = document.getElementById("tap");
+var startSound = document.getElementById("start");
+var finishSound = document.getElementById("finish");
+
+document.getElementById("diff").addEventListener("change", putScores);
 function getscores() {
-  let scores;
-  if (localStorage.getItem("scores") === null) {
-    scores = [];
-  } else {
-    scores = JSON.parse(localStorage.getItem("scores"));
+  let scores = [];
+  var diff;
+  for (var i = 0; i < 5; i++) {
+    diff = i + 1;
+    if (localStorage.getItem(`scores-${diff}`) === null) {
+      scores[i] = [];
+    } else {
+      scores[i] = JSON.parse(localStorage.getItem(`scores-${diff}`));
+    }
   }
   return scores;
 }
-
 function putScores() {
+  diff = document.getElementById("diff").value;
+  var difficulty = ["Easy", "Normal", "Intermediate", "Hard", "Expert"];
   scores = getscores();
-  scores.sort();
-  var list = document.querySelector(".scores");
-  list.innerHTML = "";
-  var l = 5 < scores.length ? 5 : scores.length;
-  for (var i = 0; i < l; i++) {
-    var min = Math.floor(scores[i] / 60000);
-    var sec = Math.round((scores[i] - min * 60000) / 1000);
+  var l = [];
+  var scbox = document.querySelector(".score-box");
+  for (var i = 0; i < 5; i++) {
+    scores[i].sort();
+    l[i] = 5 < scores[i].length ? 5 : scores[i].length;
+  }
+  scbox.innerHTML = "<h1>High Scores</h1>";
+  var k = diff - 1;
+  // for (var k = 0; k < 5; k++) {
+  var list = document.createElement("ul");
+  list.className = "scores";
+  list.innerHTML = `<li class='schead'>${difficulty[k]}</li>`;
+  for (var i = 0; i < l[k]; i++) {
+    var min = Math.floor(scores[k][i] / 60000);
+    var sec = Math.round((scores[k][i] - min * 60000) / 1000);
     var sc = document.createElement("li");
     sc.className = "score";
     sc.innerHTML = `${min} mins : ${sec} secs`;
     list.appendChild(sc);
   }
+  list.addEventListener("click", e => {});
+  scbox.appendChild(list);
+  // }
 }
 
 var box = document.getElementById("box");
-var c = 0;
-var f = 0;
+var c;
+var f;
 var lvlcount = 40;
 box.innerHTML = "<h1 class='start'>Click To Start</h1>";
 box.addEventListener("click", start);
@@ -38,15 +58,19 @@ box.addEventListener("click", start);
 //restart the game
 document.querySelector(".play-again").addEventListener("click", start);
 
+//Starting Function
+var startTime;
+
 function start() {
-  items = [];
+  c = 0;
+  f = 0;
   document.querySelector(".col-6").lastElementChild.className += " remove";
+  items = [];
   for (var i = 1; i < 21; i++) {
     items.push(i);
   }
   shuffle(items);
-  c = 0;
-  f = 0;
+  startSound.play();
   box.innerHTML = "<h1 class='start'>3</h1>";
   setTimeout(() => {
     box.innerHTML = "<h1 class='start'>2</h1>";
@@ -58,27 +82,32 @@ function start() {
     draw();
     startTime = Date.now();
   }, 3200);
+
+  lvlcount = 20 * diff;
   box.removeEventListener("click", start);
 }
-var startTime;
 
 var resbtn = document.querySelector(".reset");
 resbtn.addEventListener("click", e => {
   var scores = [];
-  localStorage.setItem("scores", JSON.stringify(scores));
+  localStorage.setItem(`scores-${diff}`, JSON.stringify(scores));
   putScores();
 });
+
+//Where Game Stops
+
 function stop() {
+  finishSound.play();
   if (startTime) {
     var endTime = Date.now();
     var difference = endTime - startTime;
     var msg = "";
     const scores = getscores();
-    if (difference < scores[0]) {
+    if (difference < scores[diff - 1][0]) {
       msg = "New HighScore<br>";
     }
-    scores.push(difference);
-    localStorage.setItem("scores", JSON.stringify(scores));
+    scores[diff - 1].push(difference);
+    localStorage.setItem(`scores-${diff}`, JSON.stringify(scores[diff - 1]));
 
     var end = document.createElement("h1");
     end.className = "";
@@ -130,6 +159,10 @@ function draw() {
     for (var l = 0; l < 5; l++) {
       var item = document.createElement("div");
 
+      item.style.background = `rgba(127,25,255,${
+        (1 / lvlcount) * items[5 * j + l]
+      })`;
+
       if (items[5 * j + l] > lvlcount) {
         item.className = "item hide";
         f++;
@@ -159,6 +192,7 @@ function draw() {
           //   items[(e.target.innerHTML - 1) % 20] += 20;
           c++;
           clickable.style.background = "#264B70";
+          tapSound.play();
           setTimeout(() => {
             draw();
           }, 100);
@@ -175,6 +209,7 @@ function draw() {
         }
         c++;
         clickable.style.background = "#264B70";
+        tapSound.play();
         setTimeout(() => {
           draw();
         }, 100);
