f75ca7275a59cb366633fe7adbb2b0171ede6836
marmikupadhyay <62810047+marmikupadhyay@users.noreply.github.com>
2020-05-02 17:22:12 +0530

    Add files via upload

--- DeltaWDTask-2/js/input.js
+++ DeltaWDTask-2/js/input.js
@@ -18,11 +18,14 @@ export default class InputHandler {
     });
 
     document.addEventListener("click", event => {
-      game.ball.speed.y = -4;
+      game.ball.speed.y = -2.5;
       game.jumpSound.play();
       if (game.ball.position.y < game.ball.gameHeight / 2) {
         game.obstacles.forEach(obstacle => {
           obstacle.speed.y = 4;
+        });
+        game.collectibles.forEach(collectible => {
+          collectible.speed.y = 4;
         });
       }
     });
